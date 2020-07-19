2dfbee2f3afd04cb9953cc2b1012c24ea9dd5719
Муравьев Алексей <teosverdi@gmail.com>
2020-07-20 00:55:21 +0300

    Ta-da

---
+++ index.js
@@ -0,0 +1,13 @@
+
+/*!
+ * is-pirog-gay <https://github.com/TeosVerdi/is-pirog-gay>
+ *
+ * Copyright (c) 2020, Teos Verdi.
+ * Released under the WTFPL License.
+ */
+
+'use strict';
+
+module.exports = function isPirogGay() {
+  return true;
+};
